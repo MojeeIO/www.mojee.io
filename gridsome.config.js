bee9485cd63836fd4d479239222826ae8c24b9a9
Özgür Uysal <ozguruysal@gmail.com>
2020-07-14 18:24:04 +0300

    Favicon and basic site settings

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,23 +6,24 @@
 const purgecss = require("@fullhuman/postcss-purgecss");
 
 const postcssPlugins = [
-  require("postcss-import"),
-  require("tailwindcss"),
-  require("postcss-nested"),
-  require("autoprefixer"),
-  ...(process.env.NODE_ENV === "production"
-    ? [purgecss(require("./purgecss.config.js"))]
-    : []),
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production"
+        ? [purgecss(require("./purgecss.config.js"))]
+        : []),
 ];
 
 module.exports = {
-  siteName: "Mojee.io",
-  plugins: [],
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      },
+    siteName: "Mojee.io",
+    siteDescription: "Lightning fast Emoji library for TypeScript & .NET",
+    plugins: [],
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: postcssPlugins,
+            },
+        },
     },
-  },
 };
