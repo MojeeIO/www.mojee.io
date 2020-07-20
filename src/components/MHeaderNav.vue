4e9f587892b94f55f28128b6e152d6f137878a54
Özgür Uysal <ozguruysal@gmail.com>
2020-07-20 16:50:47 +0300

    Emojis and pricing page components

--- src/components/MHeaderNav.vue
+++ src/components/MHeaderNav.vue
@@ -4,7 +4,7 @@
             <li v-for="item in $static.metadata.siteNav" :key="item.to">
                 <g-link
                     class="inline-flex items-center h-10 px-3 transition-colors duration-150 ease-in rounded-sm hover:text-yellow-600"
-                    active-class="text-yellow-700 bg-yellow-100 hover:text-yellow-700"
+                    exact-active-class="text-yellow-700 bg-yellow-100 hover:text-yellow-700"
                     :to="item.to"
                 >
                     {{ item.label }}
---
+++ src/pages/Pricing.vue
@@ -0,0 +1,15 @@
+<template>
+    <Layout>
+        <div class="max-w-screen-xl mx-auto">
+            <h1>Pricing page</h1>
+        </div>
+    </Layout>
+</template>
+
+<script>
+export default {
+    metaInfo: {
+        title: "Pricing",
+    },
+};
+</script>
