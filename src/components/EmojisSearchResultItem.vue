69267a5bb4534a55e16dc874a5392534e967d495
Özgür Uysal <ozguruysal@gmail.com>
2020-07-24 14:43:52 +0300

    Emoji search page: use scale effect on hover

--- src/components/EmojisSearchResultItem.vue
+++ src/components/EmojisSearchResultItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative transition-all duration-100 ease-in border border-blue-200 rounded-sm cursor-pointer md:rounded-md pb-full hover:shadow-border-2">
+    <div class="relative transition-all duration-100 ease-in transform border border-blue-200 rounded-sm cursor-pointer md:rounded-md pb-full hover:scale-105">
         <div 
             v-html="`&#x${unicode};`" 
             class="absolute inset-0 flex items-center justify-center"
