8d453750bf73195f6fd7066257dfb1938691749c
Geoffrey McGill <geoff@object.net>
2020-07-26 17:42:00 -0600

    Remove Community link for initial launch

--- mojee.config.js
+++ mojee.config.js
@@ -4,7 +4,7 @@ module.exports = {
         { label: "Emojis", to: "/emojis" },
         { label: "Pricing", to: "/pricing" },
         { label: "Docs", to: "/docs" },
-        { label: "Community", to: "https://community.mojee.io" },
+        // { label: "Community", to: "https://community.mojee.io" },
     ],
     social: [
         { id: "email", value: "hello@mojee.io" },
