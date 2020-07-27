ce1ccb3c11590a5a07ad11a9970b00809e41df1d
Geoffrey McGill <geoff@object.net>
2020-07-26 19:37:31 -0600

    Update FAQ content

--- src/components/PricingFaq.vue
+++ src/components/PricingFaq.vue
@@ -3,28 +3,25 @@
         <div class="text-center">
             <h2
                 class="text-2xl font-extrabold text-gray-900 md:text-3xl lg:text-4xl"
-            >
-                Frequently asked questions
-            </h2>
+            >Frequently asked questions</h2>
 
             <p class="max-w-2xl mx-auto mt-4">
-                We do our best to answer all of your question. If you can’t find
-                an answer below, feel free to send an
-                <a class="link" :href="`mailto:${email}`">email</a>
-                or
-                <a class="link" :href="`https://twitter.com/${twitter}`">
-                    tweet
-                </a>
-                us.
+                We do our best to answer all of your questions. If you can’t find
+                an answer below, feel free to
+                <a
+                    class="link"
+                    :href="`https://twitter.com/${twitter}`"
+                >tweet</a>, submit a GitHub
+                <a
+                    class="link"
+                    :href="`https://github.com/MojeeIO/mojee/issues`"
+                >issue</a>, or
+                <a class="link" :href="`mailto:${email}`">email</a> us.
             </p>
         </div>
 
         <pricing-faq-group class="mt-16">
-            <pricing-faq-item
-                id="q1"
-                question="Vivamus elementum semper nisi?"
-                active
-            >
+            <pricing-faq-item id="q1" question="Vivamus elementum semper nisi?" active>
                 <p>
                     Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
                     natoque penatibus et magnis dis parturient montes, nascetur
@@ -33,10 +30,7 @@
                 </p>
             </pricing-faq-item>
 
-            <pricing-faq-item
-                id="q2"
-                question="Maecenas tempus, tellus eget condimentum rhoncus?"
-            >
+            <pricing-faq-item id="q2" question="Maecenas tempus, tellus eget condimentum rhoncus?">
                 <p>
                     Donec pede justo, fringilla vel, aliquet nec, vulputate
                     eget, arcu. In enim justo, rhoncus ut, imperdiet a,
@@ -45,10 +39,7 @@
                 </p>
             </pricing-faq-item>
 
-            <pricing-faq-item
-                id="q3"
-                question="Cras ultricies mi eu turpis hendrerit fringilla?"
-            >
+            <pricing-faq-item id="q3" question="Cras ultricies mi eu turpis hendrerit fringilla?">
                 <p>
                     Vestibulum ante ipsum primis in faucibus orci luctus et
                     ultrices posuere cubilia Curae; In ac dui quis mi
@@ -56,10 +47,7 @@
                 </p>
             </pricing-faq-item>
 
-            <pricing-faq-item
-                id="q4"
-                question="Quis gravida magna mi a libero?"
-            >
+            <pricing-faq-item id="q4" question="Quis gravida magna mi a libero?">
                 <p>
                     Integer tincidunt. Cras dapibus. Vivamus elementum semper
                     nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
