83b6a1ed5b953a63f0a5afdaefe7ab4a8e8691ca
Özgür Uysal <ozguruysal@gmail.com>
2020-07-28 20:34:35 +0300

    Always center emoji detail container horizontally

--- src/components/EmojiDetail.vue
+++ src/components/EmojiDetail.vue
@@ -1,63 +1,77 @@
 <template>
-    <div
-        class="px-6 pt-12 pb-6 mx-auto md:max-w-3xl xl:max-w-screen-xl xl:pl-72 md:pt-16 md:pb-12 md:flex md:items-center"
-    >
-        <div class="flex md:items-center xs:flex-col">
-            <m-emoji
-                ref="emoji"
-                :unicode="emoji.unicode"
-                class="mb-5 text-8xl xs:text-10xl md:text-16xl lg:text-20xl"
-            />
-            <div class="items-baseline hidden mt-4 xs:flex">
-                <m-emoji :unicode="emoji.unicode" class="text-4xl md:ml-2" />
-                <m-emoji :unicode="emoji.unicode" class="text-3xl md:ml-2" />
-                <m-emoji :unicode="emoji.unicode" class="text-2xl md:ml-2" />
-                <m-emoji :unicode="emoji.unicode" class="md:ml-2" />
-            </div>
-        </div>
-
-        <div class="xs:mt-6 md:ml-12 lg:ml-12">
-            <h2
-                class="mt-1 text-xl font-bold text-gray-900 lowercase md:text-2xl"
-            >
-                {{ title }}
-            </h2>
-
-            <div class="grid items-baseline mt-6 codes-grid">
-                <div class="text-sm font-medium text-gray-900">Unicode</div>
-
-                <div class="ml-3 font-mono text-xs truncate">
-                    <span class="px-2 py-1 bg-gray-100">
-                        {{ emoji.unicode }}
-                    </span>
-                </div>
-
-                <div class="text-sm font-medium text-gray-900">Shortcode</div>
-
-                <div class="mt-3 ml-3 font-mono text-xs truncate">
-                    <span class="px-2 py-1 bg-gray-100">
-                        :{{ emoji.shortcode }}:
-                    </span>
+    <div class="flex items-center px-6 pb-10 mx-auto pt-15">
+        <div class="flex flex-col mx-auto md:flex-row md:items-center">
+            <div class="flex md:items-center xs:flex-col">
+                <m-emoji
+                    ref="emoji"
+                    :unicode="emoji.unicode"
+                    class="mb-5 text-8xl xs:text-10xl md:text-16xl lg:text-20xl"
+                />
+                <div class="items-baseline hidden mt-4 xs:flex">
+                    <m-emoji
+                        :unicode="emoji.unicode"
+                        class="text-4xl md:ml-2"
+                    />
+                    <m-emoji
+                        :unicode="emoji.unicode"
+                        class="text-3xl md:ml-2"
+                    />
+                    <m-emoji
+                        :unicode="emoji.unicode"
+                        class="text-2xl md:ml-2"
+                    />
+                    <m-emoji :unicode="emoji.unicode" class="md:ml-2" />
                 </div>
             </div>
 
-            <div class="mt-6">
-                <h4 class="font-medium text-gray-900">Copy</h4>
+            <div class="md:ml-12 lg:ml-12">
+                <h2
+                    class="mt-1 text-xl font-extrabold text-gray-900 lowercase md:text-3xl"
+                >
+                    {{ title }}
+                </h2>
 
-                <div class="flex mt-3">
-                    <emoji-copy-button label="Emoji" :copy-text="emojiChar" />
+                <div class="grid items-baseline mt-6 codes-grid md:mt-10">
+                    <div class="text-sm font-medium text-gray-900">Unicode</div>
 
-                    <emoji-copy-button
-                        class="ml-3"
-                        label="Unicode"
-                        :copy-text="emoji.unicode"
-                    />
+                    <div class="ml-3 font-mono text-xs truncate">
+                        <span class="px-2 py-1 bg-gray-100">
+                            {{ emoji.unicode }}
+                        </span>
+                    </div>
 
-                    <emoji-copy-button
-                        class="ml-3"
-                        label="Shortcode"
-                        :copy-text="`:${emoji.shortcode}:`"
-                    />
+                    <div class="text-sm font-medium text-gray-900">
+                        Shortcode
+                    </div>
+
+                    <div class="mt-3 ml-3 font-mono text-xs truncate">
+                        <span class="px-2 py-1 bg-gray-100">
+                            :{{ emoji.shortcode }}:
+                        </span>
+                    </div>
+                </div>
+
+                <div class="mt-6 md:mt-10">
+                    <h4 class="font-medium text-gray-900">Copy</h4>
+
+                    <div class="flex mt-3">
+                        <emoji-copy-button
+                            label="Emoji"
+                            :copy-text="emojiChar"
+                        />
+
+                        <emoji-copy-button
+                            class="ml-3"
+                            label="Unicode"
+                            :copy-text="emoji.unicode"
+                        />
+
+                        <emoji-copy-button
+                            class="ml-3"
+                            label="Shortcode"
+                            :copy-text="`:${emoji.shortcode}:`"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
