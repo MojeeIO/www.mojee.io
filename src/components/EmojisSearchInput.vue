0170daf13feccbd2ad29372728b04e92cc804f9e
Geoffrey McGill <geoff@object.net>
2020-08-16 23:14:10 -0600

    Trim the query value before attempting to update the query string
    Ignores an empty string value

--- src/components/EmojisSearchInput.vue
+++ src/components/EmojisSearchInput.vue
@@ -43,7 +43,7 @@ export default {
             this.updateRouteQuery({
                 name: "q",
                 defaultValue: "",
-                newValue: val,
+                newValue: val.trim(),
             });
         },
 
